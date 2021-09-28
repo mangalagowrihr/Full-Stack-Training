86790c54fd8331a697f732ad7b853694e35ba345
Mangala Gowri H R <mgowrihr88@gmail.com>
2021-09-28 18:57:54 +0530

    primitive data types

---
+++ app.js
@@ -0,0 +1,14 @@
+// String
+console.log(typeof "Hello World")
+console.log(typeof "d")
+
+// Numbers
+console.log(typeof 1)
+console.log(typeof 1.2)
+console.log(typeof 11223345667889)
+console.log(typeof -1)
+console.log(typeof -1.2)
+
+// boolean
+console.log(typeof true)
+console.log(typeof false)
