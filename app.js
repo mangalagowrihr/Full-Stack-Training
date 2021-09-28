8a8354183ac5aa657d7ecfa4f3ae5c02b97c57c7
Mangala Gowri H R <mgowrihr88@gmail.com>
2021-09-28 19:10:07 +0530

    null vs undefined

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-// String
-console.log(typeof "Hello World")
-console.log(typeof "d")
-
-// Numbers
-console.log(typeof 1)
-console.log(typeof 1.2)
-console.log(typeof 11223345667889)
-console.log(typeof -1)
-console.log(typeof -1.2)
-
-// boolean
-console.log(typeof true)
-console.log(typeof false)
+ var age=null
+//  console.log(Name)
+ console.log(typeof age)
