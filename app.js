fd70b611a395e21b0c48c887f9920796edd1eb44
Mangala Gowri H R <mgowrihr88@gmail.com>
2021-09-28 19:40:03 +0530

    operators

--- app.js
+++ app.js
@@ -1,5 +1,58 @@
-var arr=[1,2,3,4]
-console.log(arr)
-arr.push(5)
-console.log(arr)
-console.log(arr.length)
+// operators
+
+// Arithmetic operators
+
+var a=2
+var b=10
+
+console.log('a+b=',a+b)
+console.log('b-a=',b-a)
+console.log('b/a=',b/a)
+console.log('b*a=',b*a)
+console.log('b**a=',b**a)
+console.log('b%a=',b%a)
+
+// Assignment operators
+console.log('b',b)
+b+=10
+console.log('b+=10',b)
+b++
+console.log('b++',b)
+b-=10
+console.log('b-=10',b)
+b--
+console.log('b--',b)
+
+var c=10
+
+
+console.warn(c++)
+console.log(c)
+// console.warn(++c)
+
+// comparison operators
+
+var d=10
+var e=2
+var f='10'
+console.log(d>e)
+console.log(d<e)
+console.log(d>=e)
+console.log(d<=e)
+console.log(d==e)
+
+console.warn(d==f)
+console.warn(d===f)
+console.warn(d+f)
+
+// Logical operators
+
+console.log('true && true',true && true)
+console.log('true && false',true && false)
+console.log('false && true',false && true)
+console.log('false && false',false && false)
+
+console.log('true || true',true || true)
+console.log('true || false',true || false)
+console.log('false || true',false || true)
+console.log('false || false',false || false)
