1f991c0141b300ccbdd6d0b1ac62f115e7bb0ddc
mangalagowrihr <mgowrihr88@gmail.com>
2021-10-07 20:01:50 +0530

    todo list implementation

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
-// function greet(Name= "John", age = 18){
-//      console.log(Name,age)
-//      console.log('Hello')
-//     console.log('Hey' + Name + "your age is"+ age)
+// var add=(a=0,b=0)=>{
+//     return a+b
 // }
 
-// greet()
+// or
 
-
-// function add(a=0,b=0){
-//       var sum = a + b
-//       return sum
-// }
-
-// var result=add(10,20)
-// console.log(result)
-
-// var add=function(a=0,b=0){
-//    var sum=a+b
-//    return sum
-// }
-
+// var add=(a=0,b=0)=>a+b
 // console.log(add(10,20))
 
-// Arrow function
-var add=(a=0,b=0)=> {
-    var sum=a+b
-    return sum
- }
- 
- console.log(add(10,20))
+// const greet = Name => 'hi' + Name
+
+// console.log(greet('Prabhakaran'))
+
+// var arr = ['go to the GYM','Eat Food','Clean House']
+
+// for (var i=0;i<arr.length;i++){
+//     console.log(arr[i])
+// }
+
+// var callbackFunction = (element,index) => {
+//     console.log(element,index)
+// }
+
+// arr.forEach ((element,index) => {
+//     console.log(element,index)
+// })
+
+var button=document.querySelector('button')
+var input=document.querySelector('input')
+var list=document.querySelector('ul')
+
+const callbackfunc = (event) =>{
+    console.log(input.value)
+    // const inputValue=input.value
+    // const element=document.createElement('li')
+    // const textNode=document.createTextNode(inputValue)
+    // element.appendChild(textNode)
+    // list.appendChild(element)
+}
+
+button.addEventListener('click',callbackfunc)
+
+
 
 
 
