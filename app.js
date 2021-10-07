73a86ed5a5eacda2cfd1cd497ac9719623770d66
mangalagowrihr <mgowrihr88@gmail.com>
2021-10-07 21:43:32 +0530

    calculator

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
-var body=document.querySelector('body')
-var button=document.querySelector('button')
+const a=document.querySelectorAll('input')[0]
+const b=document.querySelectorAll('input')[1]
 
-const callbackfunc=(event) =>{
-   body.classList.toggle('dark')
+// const a=document.getElementsByTagName('input')[0]
+// const b=document.getElementsByTagName('input')[1]
+
+const add=document.querySelector('#add')
+const multiply=document.querySelector('#multiply')
+const resultBox=document.querySelector('.result')
+
+const sum=()=>{
+    const result=parseInt(a.value)+parseInt(b.value)
+    resultBox.innerHTML=result
 }
 
-button.addEventListener('click',callbackfunc)
+const product=()=>{
+    const result=parseInt(a.value)*parseInt(b.value)
+    resultBox.innerHTML=result
+}
+
+const calculate=(event,operation)=>{
+//    console.log(event.target)
+//   if(operation=='add'){
+//       console.log('adding')
+//   }else if (operation=='multiply'){
+//       console.log('multiply')
+//   }
+switch(operation){
+    case "add":
+        resultBox.innerHTML=parseInt(a.value)+parseInt(b.value)
+        break
+    case "multiply":
+        resultBox.innerHTML=parseInt(a.value)*parseInt(b.value)
+        break
+    default:
+        resultBox.innerHTML="Not a valid operation"
+
+}
+}
+
+add.addEventListener('click',()=>{
+    calculate(event,'add')
+})
+
+multiply.addEventListener('click',()=>{
+  
+    calculate(event,'multiply')
+})
