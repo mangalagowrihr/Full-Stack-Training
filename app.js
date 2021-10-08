07d74725307ffadfa38f7d28f75308b9de770911
mangalagowrihr <mgowrihr88@gmail.com>
2021-10-08 22:20:16 +0530

    promises

--- app.js
+++ app.js
@@ -1,106 +1,17 @@
 // 1
-// if(true) {
-//     var Name='Ferrari'
-   
-// }
-// console.log(Name)
+setTimeout( ()=>{
+    console.log("Bhavani")
+},0)
+
+console.log('Hi')
+console.log('Hey')
 
 // 2
-// for(var i=0;i<=10;i++){
-//     console.log('loop ran')
-// }
+setTimeout( ()=>{
+    console.log("Bhavani")
+},5000)
 
-// console.log(i)
+// 3
+var data=fetch('https://jsonplaceholder.typicode.com/todos/10')
 
-// if(true){
-//      var Name="ferrari"
-//     if(true){
-//         console.log(Name)
-       
-//     }
-// }
-
-// const print=()=>{
-//     var Name="Mercedes"
-//     if(true){
-//         console.log(Name)
-//     }
-//     return Name
-// }
-
-// print()
-// 
-
-// let car={
-//   name:"C Class",
-//   manufacturer:"Mercedes",
-//   print:function(){
-//      console.log(car.manufacturer,car.name)
-//   }
-// }
-
-// car.print()
-
-
-// let obj={
-//     h:"hello",
-//     w:"world",
-//     print:function(){
-//         console.log(this);
-//         console.log(this.h + this.w);
-//         console.log(obj.h);
-//     }
-// }
-
-// const temp=obj.print("hsfds");
-// console.log(temp)
-
-
-
-
-// let car={
-//     name:"C Class",
-//     manufacturer:"Mercedes",
-//     print:function(){
-//        console.log(`${this.name} was created by ${this.manufacturer}`)
-//     }
-//   }
-  
-//   car.print()
-
-
-
-
-// let car={
-//     name:"C Class",
-//     manufacturer:"Mercedes",
-//     print:() => {
-//         console.log(this)
-//        console.log(`${car.name} was created by ${car.manufacturer}`)
-//     }
-//   }
-  
-//   console.log(Object.keys(car))
-//   console.log(Object.values(car))
-
-
-
-let car={
-    name:"C Class",
-    manufacturer:"Mercedes",
-    print:() => {
-        
-       console.log(`${car.name} was created by ${car.manufacturer}`)
-    }
-  }
-  console.log(car)
-//   console.log(Object)
-//   console.log(Object)
-//   console.log(Object.keys(car))
-//   console.log(Object.values(car))
-
-// console.log(Object.hasOwnProperty('name'))
-
-console.log(car.hasOwnProperty('name'))
-
-
+console.log(data)
