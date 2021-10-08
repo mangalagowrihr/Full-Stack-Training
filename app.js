e551171864e5b295fb4f466ff4e22686627d30f8
mangalagowrihr <mgowrihr88@gmail.com>
2021-10-08 19:39:46 +0530

    todo list implementation 2

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-var arr=[1,2,3,4,5,6,7,8]
+var button=document.querySelector('button')
+var input=document.querySelector('input')
+var list=document.querySelector('ul')
 
-console.log(arr.splice(3,3))
+var chores=[]
+var deleteItem=(value)=>{
+    const index=chores.indexOf(value)
+    chores.splice(index,1)
+      console.log(chores)
+}
 
-console.log(arr)
+
+const callbackfunc = (event) =>{
+    // console.log(input.value)
+    const inputValue=input.value
+    if(chores.includes(input.value)){
+        console.log('already exists')
+    }
+    else{
+        chores.push(inputValue)
+    const element=document.createElement('li')
+    const textNode=document.createTextNode(inputValue)
+    element.appendChild(textNode)
+    list.appendChild(element)
+    element.addEventListener('click',(e)=>{e.target.remove()})
+}
+}
+
+button.addEventListener('click',callbackfunc)
