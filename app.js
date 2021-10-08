b87126f266623c0878e64e959477630789f61d63
mangalagowrihr <mgowrihr88@gmail.com>
2021-10-08 21:47:04 +0530

    scopes and objects

--- app.js
+++ app.js
@@ -1,29 +1,106 @@
-var button=document.querySelector('button')
-var input=document.querySelector('input')
-var list=document.querySelector('ul')
+// 1
+// if(true) {
+//     var Name='Ferrari'
+   
+// }
+// console.log(Name)
 
-var chores=[]
-var deleteItem=(value)=>{
-    const index=chores.indexOf(value)
-    chores.splice(index,1)
-      console.log(chores)
-}
+// 2
+// for(var i=0;i<=10;i++){
+//     console.log('loop ran')
+// }
+
+// console.log(i)
+
+// if(true){
+//      var Name="ferrari"
+//     if(true){
+//         console.log(Name)
+       
+//     }
+// }
+
+// const print=()=>{
+//     var Name="Mercedes"
+//     if(true){
+//         console.log(Name)
+//     }
+//     return Name
+// }
+
+// print()
+// 
+
+// let car={
+//   name:"C Class",
+//   manufacturer:"Mercedes",
+//   print:function(){
+//      console.log(car.manufacturer,car.name)
+//   }
+// }
+
+// car.print()
 
 
-const callbackfunc = (event) =>{
-    // console.log(input.value)
-    const inputValue=input.value
-    if(chores.includes(input.value)){
-        console.log('already exists')
+// let obj={
+//     h:"hello",
+//     w:"world",
+//     print:function(){
+//         console.log(this);
+//         console.log(this.h + this.w);
+//         console.log(obj.h);
+//     }
+// }
+
+// const temp=obj.print("hsfds");
+// console.log(temp)
+
+
+
+
+// let car={
+//     name:"C Class",
+//     manufacturer:"Mercedes",
+//     print:function(){
+//        console.log(`${this.name} was created by ${this.manufacturer}`)
+//     }
+//   }
+  
+//   car.print()
+
+
+
+
+// let car={
+//     name:"C Class",
+//     manufacturer:"Mercedes",
+//     print:() => {
+//         console.log(this)
+//        console.log(`${car.name} was created by ${car.manufacturer}`)
+//     }
+//   }
+  
+//   console.log(Object.keys(car))
+//   console.log(Object.values(car))
+
+
+
+let car={
+    name:"C Class",
+    manufacturer:"Mercedes",
+    print:() => {
+        
+       console.log(`${car.name} was created by ${car.manufacturer}`)
     }
-    else{
-        chores.push(inputValue)
-    const element=document.createElement('li')
-    const textNode=document.createTextNode(inputValue)
-    element.appendChild(textNode)
-    list.appendChild(element)
-    element.addEventListener('click',(e)=>{e.target.remove()})
-}
-}
+  }
+  console.log(car)
+//   console.log(Object)
+//   console.log(Object)
+//   console.log(Object.keys(car))
+//   console.log(Object.values(car))
 
-button.addEventListener('click',callbackfunc)
+// console.log(Object.hasOwnProperty('name'))
+
+console.log(car.hasOwnProperty('name'))
+
+
