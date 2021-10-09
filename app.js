caec89573d9c1a83fc0cfadcd495d11a8e6e74cb
mangalagowrihr <mgowrihr88@gmail.com>
2021-10-09 19:41:30 +0530

    then method after promise

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-let value = new Promise((resolve,reject)=> {
-    setTimeout(() =>{
-        resolve('Hello')
-    },3000)
+// 1
+// let response = fetch('https://jsonplaceholder.typicode.com/users')
+// console.log(response)
+
+// setTimeout(() => {
+//     const data=response
+//     console.log(data)
+// },2000)
+
+
+// 2
+// let response = fetch('https://jsonplaceholder.typicode.com/users').then(res =>{
+//     let parsedData=res.json()
+//     console.log(parsedData)
+//     return (parsedData)
+// }).then(data => {console.log(data)})
+
+
+//  or same output with less code
+let response = fetch('https://jsonplaceholder.typicode.com/users').then(res =>res.json()).then(data => {
+    console.log(data)
 })
-
-console.log(value)
-
-setTimeout(() =>{
-    console.log(value)
-},4000)
+   
 
