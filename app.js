d9487697d0aa2837930820988f1ff066f75e418e
mangalagowrihr <mgowrihr88@gmail.com>
2021-10-11 18:12:55 +0530

    promise revision 1

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-
-const getData = async(item)=>{
-    console.log(item)
-    let url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=$(item)`
-    console.log(url)
-
-    const res = await fetch(url)
-    const data = await res.json()
-    console.log()
-    data.hits.forEach(recipe =>{
-        console.log(recipe)
-    })
-}
-
-// add event listener to the button
-button.addEventListener('click',(e) => {
-    getData(input.value)
-
-   
+let greet =new Promise((resolve,reject) => {
+    setTimeout(() => {
+        resolve("Welcome")
+    },2000)
 })
+
+// console.log(greet)
+
+// setTimeout(() => {
+//     console.log(greet)
+// },6000)
+
+// instead of above we can use .then method
+
+greet.then(value =>{
+    console.log(value)
+    return "Welcome Again"
+}).then(newValue => {
+    console.log(newValue)
+})
+    
+
+
