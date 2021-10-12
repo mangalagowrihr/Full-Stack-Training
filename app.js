c5d55e515c0016f8219edcdbf4674676be8ce8d4
mangalagowrihr <mgowrihr88@gmail.com>
2021-10-12 18:18:00 +0530

    Recipe app

--- app.js
+++ app.js
@@ -6,32 +6,36 @@ const parent = document.querySelector('div.grid')
 
 const getData = async(item)=>{
     console.log(item)
-    let url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=$(item)`
+    const url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
     console.log(url)
 
     const res = await fetch(url)
     const data = await res.json()
     const recipies=data.hits
-    console.log(recipies)
+   
     recipies.forEach(item => {
-        console.log(item)
-        const template=` 
-              <article class="card">
-           <img class="card__image" src="https://i.ibb.co/RT0bjJq/food1.png" />
+      const {recipe}=item
+      
+      const {label,image,source,calories} = recipe
+      
+        const template=` <article>
+              
+           <img class="card__image" src="${image}" />
             <div class="card__data">
           <div class="card__info">
-            <h2>Nombre Comida</h2>
-            <p>Descripcion de la comida, con ingredientes</p>
+            <h2>${label}</h2>
+            <p>From ${source}</p>
           </div>
-          <h3 class="card__price">$7.50</h3>
+          <h3 class="card__price">${calories.toFixed(2)}</h3>
           <button class="card__add">+</button>
         </div>
       </article>`
 
       const newCard=document.createElement(`article`)
       newCard.setAttribute('class','card')
+      newCard.setAttribute('style','margin-bottom:50px')
       newCard.innerHTML=template
-      console.log(newCard)
+      
       parent.appendChild(newCard)
     })
   
@@ -44,3 +48,24 @@ but.addEventListener('click',(e) => {
 
    
 })
+
+
+
+
+// 2
+
+// const person=({
+//     name:"Prem",
+//     age:19,
+//     country:"India"
+// })
+
+// const{name,age,country}=person
+
+// console.log(name,age,country)
+
+// console.log(person.name)
+
+// console.log("Hi ${name} you are ${age}")
+
+
