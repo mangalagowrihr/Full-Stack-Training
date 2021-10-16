84a60667642fd733ffc77504ce17480419739867
mangalagowrihr <mgowrihr88@gmail.com>
2021-10-16 19:51:24 +0530

    tokens,authorization,middlewares

--- server.js
+++ server.js
@@ -1,22 +1,57 @@
+const e = require('express')
 const express = require('express')
 const app = express()
 const PORT = 3000
 
-const Logger = (req,res,next) =>{
-    console.log(req.method)
-    next()
-}
-
 app.use(express.json())
 
-app.get('/',(req,res) =>{
-    res.send('Server running')
+const token ="TOP_SECRET"
+let products = [{name:'iPhone12 Case',price:'999'},{name:'iPhone13 Case',price:'1199'},{name:'iPhone13 Pro Case',price:'1499'}]
+
+//Middlewares
+const validator = (req,res,next)=>{
+    const {name,price} = req.body
+
+    if(!name && !price) res.json({error:"Validation failed"})
+    else next()
+}
+
+const isAuthorized = (req,res,next)=>{
+    if(req.headers.authorisation===token) next()
+    else res.json({error:"UNAUTHORISED"})
+}
+
+//-------------PUBLIC routes------------
+//GET ROUTE
+//Send all products
+app.get('/products',(req,res) =>{
+    res.json({products})
 })
 
+//-------------PRIVATE routes------------
 
-app.post('/',(req,res) =>{
-    console.log(req.body)
-    res.send('Server running')
+app.post('/products/add',isAuthorized, validator,(req,res)=>{
+    const {name,price} = req.body
+
+    
+        products.push( {
+            name,
+            price,
+    })
+  
+    res.send({ products })
+
+})
+
+//---------AUTHENTICATION-------
+app.post('/auth',(req,res)=>{
+   const{email,password}=req.body
+   if(email==='admin@gmail.com' && password==='password'){
+       res.send({token})
+   }else{
+    res.send({ message:"UNAUTHORISED" })
+   }
+   
 })
 
 app.listen(PORT,()=>{
